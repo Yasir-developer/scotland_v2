e43546f8e36cfaa2bc31548adb2f3c44c77d22b6
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-11-08 21:36:21 +0500

    changes revert

--- config/index.js
+++ config/index.js
@@ -2,5 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "https://b29e-2406-d00-aaaa-bce1-860-75f2-cc1-bf23.ngrok-free.app"
-  : "https://scotland-v2-git-webhook-scotlandtitles.vercel.app";
-// : "https://scotland-v2.vercel.app";
+  : "https://scotland-v2.vercel.app";
