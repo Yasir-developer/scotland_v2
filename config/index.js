23a341f97452df3f72f1dca3fb8d7969d5564f0c
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-11-08 21:34:32 +0500

    config updated for webhook branch

--- config/index.js
+++ config/index.js
@@ -2,4 +2,5 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "https://b29e-2406-d00-aaaa-bce1-860-75f2-cc1-bf23.ngrok-free.app"
-  : "https://scotland-v2.vercel.app";
+  : "https://scotland-v2-git-webhook-scotlandtitles.vercel.app";
+// : "https://scotland-v2.vercel.app";
