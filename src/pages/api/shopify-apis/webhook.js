7c8345f3752b62a0d82bc8dd06f3cd1fc21e4a2e
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-11-08 21:50:50 +0500

    Update webhook.js

--- src/pages/api/shopify-apis/webhook.js
+++ src/pages/api/shopify-apis/webhook.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { server } from "../../../../config";
 export default async function webhookHandler(req, res) {
-  console.log(req.body, "request");
+  //   console.log(req.body, "request");
   // const { id, email, created_at, order_number } = req.body;
   axios
     .post(`${server}/api/order`, { data: req.body })
     .then((response) => {
+      console.log("response");
+      console.log(response);
       return res.status(200).send({ message: "success" });
     })
     .catch((error) => {
+      console.log("error.message");
+      console.log(error.message);
       return res.status(200).send({ message: error.message });
     });
   //   return res.status(200).send({ message: "error" });
