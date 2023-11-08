ef93d1384a4e38ac613aaa37230a33a65c92257b
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-11-09 00:23:07 +0500

    Update webhook.js

--- src/pages/api/shopify-apis/webhook.js
+++ src/pages/api/shopify-apis/webhook.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 import { server } from "../../../../config";
-export default function webhookHandler(req, res) {
+export default async function webhookHandler(req, res) {
   //   console.log(req.body, "request");
   // const { id, email, created_at, order_number } = req.body;
-  //   await axios
-  //     .get(`${server}/api/shopify-apis/order`, { data: req.body })
-  //     .then((response) => {
-  //       console.log("response");
-  //       console.log(response);
-  //       //   return res.status(200).send({ message: "success" });
-  //     })
-  //     .catch((error) => {
-  //       console.log("error.message");
-  //       console.log(error.message);
-  //       return res.status(200).send({ message: error.message });
-  //     });
-  axios.get(`${server}/api/shopify-apis/order`, { data: req.body });
+  await axios
+    .get(`${server}/api/shopify-apis/order`, { data: req.body })
+    .then((response) => {
+      console.log("response");
+      console.log(response);
+      //   return res.status(200).send({ message: "success" });
+    })
+    .catch((error) => {
+      console.log("error.message");
+      console.log(error.message);
+      return res.status(200).send({ message: error.message });
+    });
+  //   axios.get(`${server}/api/shopify-apis/order`, { data: req.body })
   return res.status(200).send({ message: "error" });
 }
