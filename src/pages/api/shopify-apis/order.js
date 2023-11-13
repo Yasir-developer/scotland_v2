856e1053721592a6b4d4107e9bcf5e1ece35628d
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-11-13 21:47:43 +0500

    debugging

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -137,34 +137,34 @@ export default async function handler(req, res) {
     }
   };
 
-  const db = await connectToDatabase();
-  const collection = db.collection("totalOrders");
-  console.log(req.body.id, "req.body.orderId");
-  const result = await collection.findOne({ orderId: req.body.id });
-  if (result) {
-    console.log(
-      "=========================Already Present========================="
-    );
-    return res.status(200).send({ message: "SUCCESS ALREADY PRESENT" });
-  } else {
-    console.log(result, "============result=========");
-    await axios
-      .post(`${server}/api/shopify-apis/test`, {
-        orderId: req.body.id,
-        status: true,
-      })
-      .then((response) => {
-        console.log("response");
-        console.log(response.data, "webhook response");
+  // const db = await connectToDatabase();
+  // const collection = db.collection("totalOrders");
+  // console.log(req.body.id, "req.body.orderId");
+  // const result = await collection.findOne({ orderId: req.body.id });
+  // if (result) {
+  //   console.log(
+  //     "=========================Already Present========================="
+  //   );
+  //   return res.status(200).send({ message: "SUCCESS ALREADY PRESENT" });
+  // } else {
+  //   console.log(result, "============result=========");
+  //   await axios
+  //     .post(`${server}/api/shopify-apis/test`, {
+  //       orderId: req.body.id,
+  //       status: true,
+  //     })
+  //     .then((response) => {
+  //       console.log("response");
+  //       console.log(response.data, "webhook response");
 
-        return res.status(200).send({ message: "Added in Database success" });
-      })
-      .catch((error) => {
-        console.log("error.message");
-        console.log(error.message);
-        return res.status(200).send({ message: error.message });
-      });
-  }
+  //       return res.status(200).send({ message: "Added in Database success" });
+  //     })
+  //     .catch((error) => {
+  //       console.log("error.message");
+  //       console.log(error.message);
+  //       return res.status(200).send({ message: error.message });
+  //     });
+  // }
 
   const titlePack = async (propObject) => {
     let titleConditions;
