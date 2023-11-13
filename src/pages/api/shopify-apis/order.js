71f3975e9af5b981cc321902bdcd8da14abc39bc
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-11-13 19:12:08 +0500

    Update order.js

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -31,36 +31,6 @@ export default async function handler(req, res) {
   const { id, email, created_at, order_number } = req.body;
   const { first_name, last_name } = req.body.customer;
 
-  const db = await connectToDatabase();
-  const collection = db.collection("totalOrders");
-  console.log(req.body.id, "req.body.orderId");
-  const result = await collection.findOne({ orderId: req.body.id });
-  if (result) {
-    console.log(
-      "=========================Already Present========================="
-    );
-    return res.status(200).send({ message: "SUCCESS ALREADY PRESENT" });
-  } else {
-    console.log(result, "============result=========");
-    await axios
-      .post(`${server}/api/shopify-apis/test`, {
-        orderId: req.body.id,
-        status: true,
-      })
-      .then((response) => {
-        console.log("response");
-        console.log(response.data, "webhook response");
-        emailPdfs();
-
-        return res.status(200).send({ message: "Added in Database success" });
-      })
-      .catch((error) => {
-        console.log("error.message");
-        console.log(error.message);
-        return res.status(200).send({ message: error.message });
-      });
-  }
-
   const pdfDoc = await PDFDocument.create();
   const pdfDocPrinted = await PDFDocument.create();
 
@@ -150,6 +120,36 @@ export default async function handler(req, res) {
       // toast.error(e.message);
     }
   };
+
+  const db = await connectToDatabase();
+  const collection = db.collection("totalOrders");
+  console.log(req.body.id, "req.body.orderId");
+  const result = await collection.findOne({ orderId: req.body.id });
+  if (result) {
+    console.log(
+      "=========================Already Present========================="
+    );
+    return res.status(200).send({ message: "SUCCESS ALREADY PRESENT" });
+  } else {
+    console.log(result, "============result=========");
+    await axios
+      .post(`${server}/api/shopify-apis/test`, {
+        orderId: req.body.id,
+        status: true,
+      })
+      .then((response) => {
+        console.log("response");
+        console.log(response.data, "webhook response");
+        emailPdfs();
+
+        return res.status(200).send({ message: "Added in Database success" });
+      })
+      .catch((error) => {
+        console.log("error.message");
+        console.log(error.message);
+        return res.status(200).send({ message: error.message });
+      });
+  }
 
   const titlePack = async (propObject) => {
     let titleConditions;
