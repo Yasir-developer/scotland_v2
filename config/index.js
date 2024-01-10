c515817da6397ac8ac06a8505374dc93c61c997c
Yasir Iqbal <yasirupworkdev@gmail.com>
2024-01-10 23:29:47 +0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
-  ? "https://f4d2-182-176-179-27.ngrok-free.app"
+  ? "https://3737-182-176-179-27.ngrok-free.app"
   : "https://scotland-v2.vercel.app";
